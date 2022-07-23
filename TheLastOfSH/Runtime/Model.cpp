6ca00c1147cc3c7a18d748327f6424a15bca70de
Wxz <429373960@qq.com>
2022-07-24 03:02:56 +0800

    update model

--- TheLastOfSH/Runtime/Model.cpp
+++ TheLastOfSH/Runtime/Model.cpp
@@ -4,21 +4,64 @@
 #include "assimp/Importer.hpp"
 #include "assimp/scene.h"
 #include "assimp/postprocess.h"
+
+#include <cstdint>
 namespace TheLastOfSH {
 	struct MyModel : public Model {
 
 		MyModel(const char* path) {
-			Assimp::Importer importer;
 			scene = importer.ReadFile(path, aiProcess_Triangulate);
+			processNode(scene->mRootNode, scene);
 		}
 
-		~MyModel() {
+		std::vector<Vertex> GetVertex() const {
+			return mVertex;
+		}
 
+		void processNode(aiNode* node, const aiScene* scene)
+		{
+			for (unsigned int i = 0; i < node->mNumMeshes; i++)
+			{
+				aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+				processMesh(mesh, scene);
+			}
+			// 接下来对它的子节点重复这一过程
+			for (unsigned int i = 0; i < node->mNumChildren; i++)
+			{
+				processNode(node->mChildren[i], scene);
+			}
 		}
-		virtual std::vector<Vertex> GetVertex() const {
-			return vertex;
+
+		void processMesh(aiMesh* mesh, const aiScene* scene)
+		{
+			for (unsigned int i = 0; i < mesh->mNumVertices; i++)
+			{
+				Vertex vertex{};
+				vertex.Position[0] = mesh->mVertices[i].x;
+				vertex.Position[1] = mesh->mVertices[i].y;
+				vertex.Position[2] = mesh->mVertices[i].z;
+				vertex.Normal[0] = mesh->mNormals[i].x;
+				vertex.Normal[1] = mesh->mNormals[i].y;
+				vertex.Normal[2] = mesh->mNormals[i].z;
+				vertex.TexCoords[0] = mesh->mTextureCoords[0][i].x;
+				vertex.TexCoords[1] = mesh->mTextureCoords[0][i].y;
+				
+
+				mVertex.push_back(vertex);
+			}
+
+			// 处理索引
+
+			// 处理材质
+			if (mesh->mMaterialIndex >= 0)
+			{
+			
+			}
 		}
-		std::vector<Vertex> vertex;
+		std::vector<Vertex> mVertex;
+		std::vector<uint16_t> mIndex;
+
+		Assimp::Importer importer;
 		const aiScene* scene;
 	};
 
--- TheLastOfSH/Runtime/Vertex.h
+++ TheLastOfSH/Runtime/Vertex.h
@@ -1,9 +1,12 @@
 #pragma once
 #ifdef _WIN64
-
+#include <cstdint>
 namespace TheLastOfSH {
 	struct Vertex {
-		
+		float Position[3];
+		float Normal[3];
+		float TexCoords[2];
+		uint32_t MaterialID;
 	};
 
 }
